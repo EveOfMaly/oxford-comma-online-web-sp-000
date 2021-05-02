a5e1bb28ae82bca933d57d0e71af3bd00611b256
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 16:13:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
 
 def oxford_comma(array)
   if array.length == 1 
-    array[0]
+    array.pop
   elsif array.length == 2 
      array.join(" and ")
   elsif array.length >= 3 
