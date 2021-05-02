3549e7968768943a332ad1bd5e95e5c07da399b3
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 16:09:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -18,5 +18,5 @@ end
 p oxford_comma(["kiwi"])
 p oxford_comma(["kiwi", "durian"])
 p oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
+p oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]))
 
-"kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos")
